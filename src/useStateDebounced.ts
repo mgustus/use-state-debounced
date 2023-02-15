0b78e1af443f7ac436c64f7089c95e8692a564a2
Michael Gustus <michael_g97@hotmail.com>
2023-02-16 00:44:33 +0200

    chore: init

--- src/useStateDebounced.ts
+++ src/useStateDebounced.ts
@@ -3,15 +3,14 @@ import {
 } from 'react';
 
 type ReturnType<S> = [S, S, Dispatch<SetStateAction<S>>];
-type ReturnTypeOptional<S> = ReturnType<S | undefined>;
 
 // Typescript function overloading with and without 'initialState'
 export function useStateDebounced<S>(delay: number, initialState: S | (() => S)): ReturnType<S>;
-export function useStateDebounced<S = undefined>(delay: number): ReturnTypeOptional<S>;
+export function useStateDebounced<S = undefined>(delay: number): ReturnType<S | undefined>;
 
 // Like useState but debounced. This hook allows you to debounce any fast changing value. The debounced
 // value will only reflect the latest value when the setValue has not been called for the specified time period.
-export function useStateDebounced<S>(delay: number, initialState?: S | (() => S)): ReturnTypeOptional<S> {
+export function useStateDebounced<S>(delay: number, initialState?: S | (() => S)): ReturnType<S | undefined> {
   const [value, setValue] = useState(initialState);
   const [debouncedValue, setDebouncedValue] = useState(value);
 
